b20e9afe3a78910f4e458e644e38dbd7983d54a6
hieucronc <21020318@vnu.edu.vn>
2025-03-11 23:55:36 +0700

    login, signup, header, footer

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,7 +2,7 @@ import Login from "../pages/login/Login";
 import Signup from "../pages/signup/Signup";
 
 const publicRoutes = [
-  {path: "/register", page:  Signup},
+  {path: "/signup", page:  Signup},
   {path: "/login", page: Login}
 ]
 
