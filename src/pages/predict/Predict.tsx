fc68f08329d9f0dca51e72affe7daeca31a460dd
hieucronc <21020318@vnu.edu.vn>
2025-03-12 20:32:52 +0700

    profile page

---
+++ src/pages/predict/Predict.tsx
@@ -0,0 +1,18 @@
+// import { useState } from "react";
+// import { Link } from "react-router-dom";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import "./predict.scss";
+
+const Predict = () => {
+ 
+
+  return (<>
+    <Header />
+    
+    <Footer/>
+  </>
+  );
+};
+
+export default Predict;
