eb0baf3d7198f8c5d9af5cc019a53dc975af7a00
TrieuThanhTung <tung.ttrieu@gmail.com>
2025-03-17 22:45:34 +0700

    fix css hero

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -3,6 +3,7 @@ import './hero.scss';
 import heroBackground from '../../assets/images/hero-background.jpg';
 import DirectionsCarRoundedIcon from '@mui/icons-material/DirectionsCarRounded';
 import CalculateRoundedIcon from '@mui/icons-material/CalculateRounded';
+import { Link } from "react-router-dom";
 
 const Hero: React.FC = () => {
   return (
@@ -11,14 +12,14 @@ const Hero: React.FC = () => {
         <h2>Mua Bán Xe Cũ Dễ Dàng & Nhanh Chóng</h2>
         <p>Nền tảng uy tín với công nghệ định giá AI</p>
         <div className="hero-buttons">
-          <button className="btn primary">
+          <div className="btn primary">
             <CalculateRoundedIcon className="icon"/>
-            Định giá ngay
-          </button>
-          <button className="btn secondary">
+            <Link to="">Định giá ngay</Link>
+          </div>
+          <div className="btn secondary">
             <DirectionsCarRoundedIcon className="icon"/>
-            <span>Danh sách xe</span>
-          </button>
+            <Link to="">Danh sách xe</Link>
+          </div>
         </div>
       </div>
     </section>
