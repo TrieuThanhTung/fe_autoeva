a146a174ae6016e28c1f323276834c694ac66683
TrieuThanhTung <tung.ttrieu@gmail.com>
2025-03-19 22:53:49 +0700

    add layout

---
+++ src/pages/post/PostDetailPage.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from "./PostDetailPage.module.scss";
+
+const PostDetailPage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.mainContent}>
+        {/* <PostHeader />
+        <PostGallery />
+        <PostDetails />
+        <PostDescription />
+        <CommentSection /> */}
+      </div>
+      <div className={styles.sidebar}>
+
+      </div>
+    </div>
+  );
+}
+
+export default PostDetailPage
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,5 +1,6 @@
 import Home from "../pages/home/Home";
 import Login from "../pages/login/Login";
+import PostDetailPage from "../pages/post/PostDetailPage";
 import Predict from "../pages/predict/Predict";
 import Profile from "../pages/profile/Profile";
 import Signup from "../pages/signup/Signup";
@@ -12,7 +13,8 @@ const authLayoutRoutes = [
 const mainLayoutRoutes = [
   {path: "/", page: Home},
   {path: "/predict", page: Predict},
-  {path: "/profile", page: Profile}
+  {path: "/profile", page: Profile},
+  {path: "/post", page: PostDetailPage}
 ]
 
 export {authLayoutRoutes, mainLayoutRoutes}
