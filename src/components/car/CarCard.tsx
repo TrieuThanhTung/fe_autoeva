e75e8afe2e8217af7afbc1e6e23537c82b00114e
TrieuThanhTung <tung.ttrieu@gmail.com>
2025-03-20 21:13:52 +0700

    fix css

--- src/components/car/CarCard.tsx
+++ src/components/car/CarCard.tsx
@@ -1,5 +1,7 @@
 import "./CarCard.scss";
 import React from "react";
+import { FaLocationDot } from "react-icons/fa6";
+import { FaRoad } from "react-icons/fa";
 
 interface CarCardProps {
   name: string;
@@ -17,9 +19,9 @@ const CarCard: React.FC<CarCardProps> = ({ name, image, price, location, mileage
       </div>
       <div className="card-content">
         <h3 className="font-bold mt-5">{name}</h3>
-        <p className="basic-info">
-          📍 {location} • 🚗 {mileage}
-        </p>
+        <span className="basic-info">
+          <FaRoad className="icon"/> {location} • <FaLocationDot className="icon"/> {mileage}
+        </span>
         <div className="card-content-footer">
           <span className="price">{price} đ</span>
           <button className="mt-2 bg-black text-white px-4 py-2 rounded-md">Xem chi tiết</button>
