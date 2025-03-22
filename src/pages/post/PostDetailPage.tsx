a6bee566a30aa83bf0e939da6ee83f435c024c7e
TrieuThanhTung <tung.ttrieu@gmail.com>
2025-03-22 23:37:43 +0700

    add selling infor

--- src/pages/post/PostDetailPage.tsx
+++ src/pages/post/PostDetailPage.tsx
@@ -4,6 +4,7 @@ import PostImages from '../../components/postDetails/postImages/PostImages';
 import PostDetail from '../../components/postDetails/postDetail/PostDetail';
 import { data_details, images } from '../../util/data';
 import CommentSection from '../../components/postDetails/CommentSection/CommentSection';
+import SellerInfo from '../../components/postDetails/SellerInfo/SellerInfo';
 
 const PostDetailPage = () => {
 
@@ -15,11 +16,16 @@ const PostDetailPage = () => {
           <PostDetail {...data_details} />
         </div>
         <div className={styles.commentContainer}>
-          <CommentSection /> 
+          <CommentSection />
         </div>
       </div>
       <div className={styles.sidebar}>
-        
+        <SellerInfo
+          name="Nguyễn Văn A"
+          avatar="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg"
+          joinYear={2020}
+          phone="0912.345.678"
+        />
       </div>
     </div>
   );
