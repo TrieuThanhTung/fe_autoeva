4a631c52a21ab77063ef7c7006ac70b75ab4bc1d
TrieuThanhTung <tung.ttrieu@gmail.com>
2025-04-05 09:37:03 +0700

    add dist

---
+++ src/api/AxiosInstance.tsx
@@ -0,0 +1,33 @@
+
+import axios from "axios";
+
+const API_BASE_URL = "http://localhost:3001";
+
+const axiosInstance = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+// Xử lý lỗi response (tùy chọn)
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error("API Error:", error);
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
