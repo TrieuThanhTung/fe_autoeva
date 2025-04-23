67c59bd7bad4030f4bb42426a6226ca4e5a69dde
TrieuThanhTung <tung.ttrieu@gmail.com>
2025-04-23 22:47:47 +0700

    fix logout

--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -32,7 +32,7 @@ export const AuthContextProvider: React.FC<{children: ReactNode}> = ({ children 
   };
 
   const logout = () => {
-    localStorage.removeItem("access_token");
+    localStorage.removeItem("access-token");
     localStorage.removeItem("uid");
     localStorage.removeItem("client");
     setLoggedIn(false);
